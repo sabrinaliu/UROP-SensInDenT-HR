352fc9ff5e17e9827a5b4808c05dd34afa4d8949
sabrinaliu <sliu.owl68@yahoo.com>
2021-01-10 22:15:59 -0500

    Add units to BA plots

--- util/blandAltman.m
+++ util/blandAltman.m
@@ -1,14 +1,20 @@
-function blandAltman(ref, exp, relScore)
+function blandAltman(ref, exp, relScore, timeInterval)
 % Given sequence of reference values and sequence of experimental values,
 % make Bland-Altman plot
 
     err = exp - ref;
 
-    if nargin < 3
+    if nargin < 3 || numel(relScore) == 0
         scatter(ref, err)
     else
         scatter(ref, err, [], relScore)
         colorbar
+    end
+    
+    if nargin < 4 || ~timeInterval
+        units = "bpm";
+    else
+        units = "ms";
     end
 
 
@@ -23,7 +29,7 @@ function blandAltman(ref, exp, relScore)
     yline(mu, '--g', "Mean: " + num2str(mu), "FontSize", 12);
     yline(mu + sig*1.96, '-.r', "+1.96sd: " + num2str(mu + sig*1.96), "FontSize", 12);
     yline(mu - sig*1.96, '-.r', "-1.96sd: " + num2str(mu - sig*1.96), "FontSize", 12);
-    xlabel('Reference', "FontSize", 16)
-    ylabel('Estimate - Reference', "FontSize", 16)
+    xlabel("Reference (" + units + ")", "FontSize", 16)
+    ylabel("Estimate - Reference (" + units + ")", "FontSize", 16)
 
 end
